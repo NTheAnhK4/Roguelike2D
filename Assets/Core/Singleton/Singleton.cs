c12e2870b2063c6f8a71a9cb7b56b3036a0c81b1
Nguyen The Anh <nguyentheanh14102004@gmail.com>
2025-01-15 09:15:17 +0700

    Start game

---
+++ Assets/Core/Singleton/Singleton.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+    public static T Instance => instance;
+
+    protected void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.Log("Only one " + typeof(T) + " allows to exist");
+        }
+
+        
+        instance = (T)(MonoBehaviour)this;
+    }
+}
